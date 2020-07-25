92188ae0b8c85a9a36ba0837bf7de6ef021d50f7
Ivan <ivankuz@hotmail.com>
2020-07-25 00:25:27 -0700

    var-> let, renaming input

--- jquery.a-tools-1.4.1.js
+++ jquery.a-tools-1.4.1.js
@@ -11,7 +11,7 @@
     <input type="text" />
 
     // Get current selection
-    var sel = $("textarea").getSelection()
+    let sel = $("textarea").getSelection()
     
     // Replace current selection
     $("input").replaceSelection("foo");
@@ -40,20 +40,20 @@
     $("#textArea").setSelection(10,15);
 
  */
-var caretPositionAmp;
+let caretPositionAmp;
 
 jQuery.fn.extend({
   getSelection: function () {
     // function for getting selection, and position of the selected text
-    var input = this.jquery ? this[0] : this;
-    var start;
-    var end;
-    var part;
+    const textareaElement = this[0];
+    let start;
+    let end;
+    let part;
 
-    if (typeof input.selectionStart == 'number') {
-      start = input.selectionStart;
-      end = input.selectionEnd;
-      part = input.value.substring(input.selectionStart, input.selectionEnd);
+    if (typeof textareaElement.selectionStart == 'number') {
+      start = textareaElement.selectionStart;
+      end = textareaElement.selectionEnd;
+      part = textareaElement.value.substring(textareaElement.selectionStart, textareaElement.selectionEnd);
       return { start: start, end: end, text: part, length: end - start };
     } else {
       return { start: undefined, end: undefined, text: undefined, length: undefined };
@@ -62,21 +62,21 @@ jQuery.fn.extend({
 
   // function for the replacement of the selected text
   replaceSelection: function (inputStr) {
-    var input = this.jquery ? this[0] : this;
-    var start;
-    var end;
-    var position = 0;
-    var mozScrollFix = input.scrollTop == undefined ? 0 : input.scrollTop;
+    const textareaElement = this[0];
+    let start;
+    let end;
+    let position = 0;
+    let mozScrollFix = textareaElement.scrollTop == undefined ? 0 : textareaElement.scrollTop;
     if (
-      typeof input.selectionStart == 'number' && // MOZILLA support
-      input.selectionStart != input.selectionEnd
+      typeof textareaElement.selectionStart == 'number' && // MOZILLA support
+      textareaElement.selectionStart != textareaElement.selectionEnd
     ) {
-      start = input.selectionStart;
-      end = input.selectionEnd;
-      input.value = input.value.substr(0, start) + inputStr + input.value.substr(end);
+      start = textareaElement.selectionStart;
+      end = textareaElement.selectionEnd;
+      textareaElement.value = textareaElement.value.substr(0, start) + inputStr + textareaElement.value.substr(end);
       position = start + inputStr.length;
-      input.setSelectionRange(position, position);
-      input.scrollTop = mozScrollFix;
+      textareaElement.setSelectionRange(position, position);
+      textareaElement.scrollTop = mozScrollFix;
       return this;
     }
     return this;
@@ -87,10 +87,10 @@ jQuery.fn.extend({
     startPosition = parseInt(startPosition);
     endPosition = parseInt(endPosition);
 
-    var input = this.jquery ? this[0] : this;
-    input.focus();
-    if (typeof input.selectionStart != 'number') {
-      re = input.createTextRange();
+    const textareaElement = this[0];
+    textareaElement.focus();
+    if (typeof textareaElement.selectionStart != 'number') {
+      re = textareaElement.createTextRange();
       if (re.text.length < endPosition) {
         endPosition = re.text.length + 1;
       }
@@ -99,15 +99,15 @@ jQuery.fn.extend({
       return this;
     }
     if (document.selection) {
-      var number = 0;
-      var plus = 0;
-      var position = 0;
-      var plusEnd = 0;
+      let number = 0;
+      let plus = 0;
+      let position = 0;
+      let plusEnd = 0;
 
-      if (typeof input.selectionStart == 'number') {
+      if (typeof textareaElement.selectionStart == 'number') {
         if (number > 0) {
-          for (var i = 0; i <= number; i++) {
-            var w = input.value.indexOf('\n', position);
+          for (let i = 0; i <= number; i++) {
+            let w = textareaElement.value.indexOf('\n', position);
             if (w != -1 && w < startPosition) {
               position = w + 1;
               plus++;
@@ -128,43 +128,43 @@ jQuery.fn.extend({
         }
         startPosition = startPosition + plus;
         endPosition = endPosition + plusEnd;
-        input.selectionStart = startPosition;
-        input.selectionEnd = endPosition;
+        textareaElement.selectionStart = startPosition;
+        textareaElement.selectionEnd = endPosition;
         return this;
       } else {
         return this;
       }
-    } else if (input.selectionStart) {
+    } else if (textareaElement.selectionStart) {
       // MOZILLA support
-      input.focus();
-      input.selectionStart = startPosition;
-      input.selectionEnd = endPosition;
+      textareaElement.focus();
+      textareaElement.selectionStart = startPosition;
+      textareaElement.selectionEnd = endPosition;
       return this;
     }
   },
 
   // insert text at current caret position
   insertAtCaretPos: function (inputStr) {
-    var input = this.jquery ? this[0] : this;
-    var start;
-    var end;
-    var position;
-    var s;
-    var re;
-    var rc;
-    var point;
-    var minus = 0;
-    var number = 0;
-    var mozScrollFix = input.scrollTop == undefined ? 0 : input.scrollTop;
-    input.focus();
-    if (document.selection && typeof input.selectionStart != 'number') {
-      if (input.value.match(/\n/g) != null) {
-        number = input.value.match(/\n/g).length; // number of EOL simbols
+    const textareaElement = this[0];
+    let start;
+    let end;
+    let position;
+    let s;
+    let re;
+    let rc;
+    let point;
+    let minus = 0;
+    let number = 0;
+    let mozScrollFix = textareaElement.scrollTop == undefined ? 0 : textareaElement.scrollTop;
+    textareaElement.focus();
+    if (document.selection && typeof textareaElement.selectionStart != 'number') {
+      if (textareaElement.value.match(/\n/g) != null) {
+        number = textareaElement.value.match(/\n/g).length; // number of EOL simbols
       }
       point = parseInt(caretPositionAmp);
       if (number > 0) {
-        for (var i = 0; i <= number; i++) {
-          var w = input.value.indexOf('\n', position);
+        for (let i = 0; i <= number; i++) {
+          let w = textareaElement.value.indexOf('\n', position);
           if (w != -1 && w <= point) {
             position = w + 1;
             point = point - 1;
@@ -175,12 +175,12 @@ jQuery.fn.extend({
     }
     caretPositionAmp = parseInt(caretPositionAmp);
 
-    if (document.selection && typeof input.selectionStart != 'number') {
+    if (document.selection && typeof textareaElement.selectionStart != 'number') {
       s = document.selection.createRange();
       if (s.text.length != 0) {
         return this;
       }
-      re = input.createTextRange();
+      re = textareaElement.createTextRange();
       textLength = re.text.length;
       rc = re.duplicate();
       re.moveToBookmark(s.getBookmark());
@@ -235,19 +235,19 @@ jQuery.fn.extend({
         caretPositionAmp = caretPositionAmp + inputStr.length;
       }
       s.text = inputStr;
-      input.focus();
+      textareaElement.focus();
 
       return this;
     } else if (
-      typeof input.selectionStart == 'number' && // MOZILLA support
-      input.selectionStart == input.selectionEnd
+      typeof textareaElement.selectionStart == 'number' && // MOZILLA support
+      textareaElement.selectionStart == textareaElement.selectionEnd
     ) {
-      position = input.selectionStart + inputStr.length;
-      start = input.selectionStart;
-      end = input.selectionEnd;
-      input.value = input.value.substr(0, start) + inputStr + input.value.substr(end);
-      input.setSelectionRange(position, position);
-      input.scrollTop = mozScrollFix;
+      position = textareaElement.selectionStart + inputStr.length;
+      start = textareaElement.selectionStart;
+      end = textareaElement.selectionEnd;
+      textareaElement.value = textareaElement.value.substr(0, start) + inputStr + textareaElement.value.substr(end);
+      textareaElement.setSelectionRange(position, position);
+      textareaElement.scrollTop = mozScrollFix;
       return this;
     }
     return this;
@@ -255,27 +255,31 @@ jQuery.fn.extend({
 
   // Set caret position
   setCaretPos: function (inputStr) {
-    var input = this.jquery ? this[0] : this;
-    var s;
-    var re;
-    var position;
-    var number = 0;
-    var minus = 0;
-    var w;
-    input.focus();
+    const textareaElement = this[0];
+    let s;
+    let re;
+    let position;
+    let number = 0;
+    let minus = 0;
+    let w;
+    textareaElement.focus();
     if (parseInt(inputStr) == 0) {
       return this;
     }
     //if (document.selection && typeof(input.selectionStart) == "number") {
     if (parseInt(inputStr) > 0) {
       inputStr = parseInt(inputStr) - 1;
-      if (document.selection && typeof input.selectionStart == 'number' && input.selectionStart == input.selectionEnd) {
-        if (input.value.match(/\n/g) != null) {
-          number = input.value.match(/\n/g).length; // number of EOL simbols
+      if (
+        document.selection &&
+        typeof textareaElement.selectionStart == 'number' &&
+        textareaElement.selectionStart == textareaElement.selectionEnd
+      ) {
+        if (textareaElement.value.match(/\n/g) != null) {
+          number = textareaElement.value.match(/\n/g).length; // number of EOL simbols
         }
         if (number > 0) {
-          for (var i = 0; i <= number; i++) {
-            w = input.value.indexOf('\n', position);
+          for (let i = 0; i <= number; i++) {
+            w = textareaElement.value.indexOf('\n', position);
             if (w != -1 && w <= inputStr) {
               position = w + 1;
               inputStr = parseInt(inputStr) + 1;
@@ -285,14 +289,14 @@ jQuery.fn.extend({
       }
     } else if (parseInt(inputStr) < 0) {
       inputStr = parseInt(inputStr) + 1;
-      if (document.selection && typeof input.selectionStart != 'number') {
-        inputStr = input.value.length + parseInt(inputStr);
-        if (input.value.match(/\n/g) != null) {
-          number = input.value.match(/\n/g).length; // number of EOL simbols
+      if (document.selection && typeof textareaElement.selectionStart != 'number') {
+        inputStr = textareaElement.value.length + parseInt(inputStr);
+        if (textareaElement.value.match(/\n/g) != null) {
+          number = textareaElement.value.match(/\n/g).length; // number of EOL simbols
         }
         if (number > 0) {
-          for (var i = 0; i <= number; i++) {
-            w = input.value.indexOf('\n', position);
+          for (let i = 0; i <= number; i++) {
+            w = textareaElement.value.indexOf('\n', position);
             if (w != -1 && w <= inputStr) {
               position = w + 1;
               inputStr = parseInt(inputStr) - 1;
@@ -301,15 +305,15 @@ jQuery.fn.extend({
           }
           inputStr = inputStr + minus - number;
         }
-      } else if (document.selection && typeof input.selectionStart == 'number') {
-        inputStr = input.value.length + parseInt(inputStr);
-        if (input.value.match(/\n/g) != null) {
-          number = input.value.match(/\n/g).length; // number of EOL simbols
+      } else if (document.selection && typeof textareaElement.selectionStart == 'number') {
+        inputStr = textareaElement.value.length + parseInt(inputStr);
+        if (textareaElement.value.match(/\n/g) != null) {
+          number = textareaElement.value.match(/\n/g).length; // number of EOL simbols
         }
         if (number > 0) {
           inputStr = parseInt(inputStr) - number;
-          for (var i = 0; i <= number; i++) {
-            w = input.value.indexOf('\n', position);
+          for (let i = 0; i <= number; i++) {
+            w = textareaElement.value.indexOf('\n', position);
             if (w != -1 && w <= inputStr) {
               position = w + 1;
               inputStr = parseInt(inputStr) + 1;
@@ -318,98 +322,26 @@ jQuery.fn.extend({
           }
         }
       } else {
-        inputStr = input.value.length + parseInt(inputStr);
+        inputStr = textareaElement.value.length + parseInt(inputStr);
       }
     } else {
       return this;
     }
     if (
-      typeof input.selectionStart == 'number' && // MOZILLA support
-      input.selectionStart == input.selectionEnd
+      typeof textareaElement.selectionStart == 'number' && // MOZILLA support
+      textareaElement.selectionStart == textareaElement.selectionEnd
     ) {
-      input.setSelectionRange(inputStr, inputStr);
+      textareaElement.setSelectionRange(inputStr, inputStr);
       return this;
     }
     return this;
   },
 
   countCharacters: function (str) {
-    var input = this.jquery ? this[0] : this;
-    if (input.value.match(/\r/g) != null) {
-      return input.value.length - input.value.match(/\r/g).length;
+    const textareaElement = this[0];
+    if (textareaElement.value.match(/\r/g) != null) {
+      return textareaElement.value.length - textareaElement.value.match(/\r/g).length;
     }
-    return input.value.length;
-  },
-
-  setMaxLength: function (max, f) {
-    this.each(function () {
-      var input = this.jquery ? this[0] : this;
-      var type = input.type;
-      var isSelected;
-      var maxCharacters;
-      // remove limit if input is a negative number
-      if (parseInt(max) < 0) {
-        max = 100000000;
-      }
-      if (type == 'text') {
-        input.maxLength = max;
-      }
-      if (type == 'textarea' || type == 'text') {
-        input.onkeypress = function (e) {
-          var spacesR = input.value.match(/\r/g);
-          maxCharacters = max;
-          if (spacesR != null) {
-            maxCharacters = parseInt(maxCharacters) + spacesR.length;
-          }
-          // get event
-          var key = e || event;
-          var keyCode = key.keyCode;
-          // check if any part of text is selected
-          if (document.selection) {
-            isSelected = document.selection.createRange().text.length > 0;
-          } else {
-            isSelected = input.selectionStart != input.selectionEnd;
-          }
-          if (
-            input.value.length >= maxCharacters &&
-            (keyCode > 47 || keyCode == 32 || keyCode == 0 || keyCode == 13) &&
-            !key.ctrlKey &&
-            !key.altKey &&
-            !isSelected
-          ) {
-            input.value = input.value.substring(0, maxCharacters);
-            if (typeof f == 'function') {
-              f();
-            } //callback function
-            return false;
-          }
-        };
-        input.onkeyup = function () {
-          var spacesR = input.value.match(/\r/g);
-          var plus = 0;
-          var position = 0;
-          maxCharacters = max;
-          if (spacesR != null) {
-            for (var i = 0; i <= spacesR.length; i++) {
-              if (input.value.indexOf('\n', position) <= parseInt(max)) {
-                plus++;
-                position = input.value.indexOf('\n', position) + 1;
-              }
-            }
-            maxCharacters = parseInt(max) + plus;
-          }
-          if (input.value.length > maxCharacters) {
-            input.value = input.value.substring(0, maxCharacters);
-            if (typeof f == 'function') {
-              f();
-            }
-            return this;
-          }
-        };
-      } else {
-        return this;
-      }
-    });
-    return this;
+    return textareaElement.value.length;
   },
 });
